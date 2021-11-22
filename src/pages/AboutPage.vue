a6621f3a675a2b80abe708e466806bce20c008bc
박현우 <phu54321@naver.com>
2021-11-22 16:36:58 +0900

    feat((AboutPage): cancel button now on title bar.

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -16,17 +16,15 @@ export default {
 <div class="modal-card">
   <header class="modal-card-head">
     <p class="modal-card-title">About</p>
+    <button class="delete" aria-label="close" @click='$emit("close")'></button>
   </header>
-  <section class="modal-card-body">
+  <section class="modal-card-body modal-card-foot">
     <div class="content">
       <div class="title">Trgk's Audio Blind Test</div>
       <div class="subtitle">Version {{version}}</div>
       <p>이중맹검 블라인드테스트를 도와주는 프로그램입니다.</p>
     </div>
   </section>
-  <footer class="modal-card-foot">
-    <button class="button" @click='$emit("close")'>OK</button>
-  </footer>
 </div>
 
 </template>
