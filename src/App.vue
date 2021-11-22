80468499b0e21b0c80c2ee8cb9e60318be2a8a2b
박현우 <phu54321@naver.com>
2021-11-22 16:06:54 +0900

    feat: added 'select file'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <script setup lang="ts">
 import './scss/styles.scss'
 import 'animate.css/animate.css'
-import { ref } from 'vue'
-import BlindTest from './pages/BlindTest/index.vue'
+import { ref, watch } from 'vue'
 import BulmaModal from './utils/BulmaModal.vue'
 import AboutPage from './pages/AboutPage.vue'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
+import calibrateBase64 from './calibrate.zip.base64?raw'
+import { BlindTest, loadBlindTest } from './pages/BlindTest/blindTestData'
+import BlindTestVue from './pages/BlindTest/TestMain.vue'
+import SelectFile from './pages/SelectFile.vue'
+
+// This is where the application will put zipb64 data in.
+const start = '!@#$%'
+const end = '^&*()'
+const data = '!@#$%^&*()'
+
+// Main logic begins
+const zipb64 = ref(data.slice(start.length, data.length - end.length))
+const blindTest = ref(null as null | BlindTest)
+
+async function onZipLoad () {
+  let b64 = zipb64.value
+  if (b64.length === 0) b64 = calibrateBase64
+  blindTest.value = await loadBlindTest(b64)
+}
+
+watch(zipb64, onZipLoad)
+onZipLoad()
+
+// ---------------------------
+
+// Menu expand / collapse
 const isMenuExpanded = ref(false)
 function toggleMenu () {
   isMenuExpanded.value = !isMenuExpanded.value
 }
 
+function _arrayBufferToBase64 (buffer: ArrayBuffer) {
+  let binary = ''
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return window.btoa(binary)
+}
+
+function onFileSelect (file: File) {
+  const reader = new FileReader()
+  reader.onload = function () {
+    zipb64.value = _arrayBufferToBase64(reader.result as ArrayBuffer)
+  }
+  reader.readAsArrayBuffer(file)
+  showCreateNewTest.value = false
+  blindTest.value = null
+}
+
+// ---------------------------
+// Modals
+
 const showAbout = ref(false)
+const showCreateNewTest = ref(false)
 
 </script>
 
@@ -29,7 +80,7 @@ const showAbout = ref(false)
   </div>
   <div class="navbar-menu" :class='{"is-active": isMenuExpanded}'>
     <div class="navbar-end">
-      <a class="navbar-item">Create new test</a>
+      <a class="navbar-item" @click='showCreateNewTest = true'>테스트 제작</a>
       <a class="navbar-item" @click='showAbout = true'>About</a>
       <a class="navbar-item" href='https://github.com/phu54321/tabt' target="blank">
         <font-awesome-icon class="mr-1" :icon="['fab', 'github']"></font-awesome-icon>Github
@@ -37,12 +88,23 @@ const showAbout = ref(false)
     </div>
   </div>
 </nav>
+
 <div class="container p-3">
-  <BlindTest />
+  <Loading :active="blindTest === null">
+    <template v-slot:after>
+      <p class="has-text-centered">Loading...</p>
+    </template>
+  </Loading>
+
+  <BlindTestVue v-if='blindTest !== null' :blind-test='blindTest' />
 </div>
 
 <bulma-modal :show='showAbout'>
   <AboutPage @close='showAbout = false' />
+</bulma-modal>
+
+<bulma-modal :show='showCreateNewTest'>
+  <SelectFile @close='showCreateNewTest = false' @select="onFileSelect" />
 </bulma-modal>
 
 </template>
@@ -52,4 +114,9 @@ const showAbout = ref(false)
   font-family: sans-serif;
   user-select: none;
 }
+
+.vld-icon {
+  text-align: center;
+}
+
 </style>
---
+++ src/pages/SelectFile.vue
@@ -0,0 +1,46 @@
+<script setup lang='ts'>
+
+// eslint-disable-next-line func-call-spacing
+const emit = defineEmits<{
+  (eventName: 'select', file: File): void
+  (eventName: 'close'): void
+}>()
+
+function onFileSelect (e: Event) {
+  const el = e.target as HTMLInputElement
+  if (el.files && el.files.length > 0) {
+    emit('select', el.files[0])
+  }
+}
+
+</script>
+
+<template>
+
+<div class="modal-card">
+  <header class="modal-card-head">
+    <p class="modal-card-title">새 테스트 제작</p>
+  </header>
+  <section class="modal-card-body">
+    <div class="content">
+      <div class="file">
+        <label class="file-label">
+          <input ref='fileInput' @change='onFileSelect' class="file-input" type="file" name="resume" accept=".zip">
+          <span class="file-cta">
+            <span class="file-icon">
+              <font-awesome-icon icon='upload' />
+              <i class="fas fa-upload"></i>
+            </span>
+            <span class="file-label">
+              파일 선택
+            </span>
+          </span>
+        </label>
+      </div>    </div>
+  </section>
+  <footer class="modal-card-foot">
+    <button class="button" @click='$emit("close")'>OK</button>
+  </footer>
+</div>
+
+</template>
