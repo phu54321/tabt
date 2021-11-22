edcad3be3f924d1206e89965e1011fef6d0bc69c
박현우 <phu54321@naver.com>
2021-11-22 14:57:31 +0900

    feat: added single file export option.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,20 @@ export default defineConfig({
     vue(),
     eslintPlugin({
       fix: true
-    })
-  ]
+    }),
+    viteSingleFile()
+  ],
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
+    rollupOptions: {
+      inlineDynamicImports: true,
+      output: {
+        manualChunks: () => 'everything.js'
+      }
+    }
+  }
 })
