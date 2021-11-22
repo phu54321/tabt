e55d3650e24d6e8bd3efac27c89f526f8fbe5c48
박현우 <phu54321@naver.com>
2021-11-22 14:51:18 +0900

    feat: preparation for single file html export

--- src/pages/BlindTest/index.vue
+++ src/pages/BlindTest/index.vue
@@ -1,10 +1,19 @@
 <script setup lang='ts'>
 import { ref } from 'vue'
-import zipb64 from '../../../testData/thdtest.zip.base64?raw'
 import { BlindTest, loadBlindTest } from './blindTestData'
 import TestMain from './TestMain.vue'
 import Loading from 'vue-loading-overlay'
+import calibrateBase64 from './../../calibrate.zip.base64?raw'
 import 'vue-loading-overlay/dist/vue-loading.css'
+
+// This is where the application will put zipb64 data in.
+const start = '!@#$%'
+const end = '^&*()'
+const data = '!@#$%^&*()'
+
+// Main logic begins
+let zipb64 = data.slice(start.length, data.length - end.length)
+if (zipb64.length === 0) zipb64 = calibrateBase64
 
 const blindTest = ref(null as null | BlindTest)
 loadBlindTest(zipb64).then(b => {
