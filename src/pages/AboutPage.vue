2af28284a8e22d8312fcb622f88c58356dca1e10
박현우 <phu54321@naver.com>
2021-11-24 10:59:15 +0900

    feat: add youtube help

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -18,13 +18,36 @@ export default {
     <p class="modal-card-title">About</p>
     <button class="delete" aria-label="close" @click='$emit("close")'></button>
   </header>
-  <section class="modal-card-body modal-card-foot">
+  <section class="modal-card-body">
     <div class="content">
       <div class="title">Trgk's Audio Blind Test</div>
       <div class="subtitle">Version {{version}}</div>
       <p>이중맹검 블라인드테스트를 도와주는 프로그램입니다.</p>
     </div>
   </section>
+    <footer class="modal-card-foot">
+      <div class="youtube-wrapper">
+        <iframe src="https://www.youtube.com/embed/qtg3AmN2IuQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </footer>
+
 </div>
 
 </template>
+
+<style lang='scss'>
+
+.youtube-wrapper {
+  position: relative;
+  padding-bottom: 56.25%;
+  width: 100%;
+
+  iframe {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
