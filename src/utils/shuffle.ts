e3e813f508c7556ec595943ee67ac31bea3f2370
박현우 <phu54321@naver.com>
2025-04-13 17:47:31 +0900

    fix: shuffle.ts should be in-place

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,14 +1,13 @@
 /**
- * Shuffles array in place.
+ * Shuffles array in-place
  * @param {Array} a items An array containing the items.
  */
 export function shuffle <T> (a: T[]) {
-  const ret = a.slice()
-  for (let i = ret.length - 1; i > 0; i--) {
+  for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    const x = ret[i]
-    ret[i] = ret[j]
-    ret[j] = x
+    const x = a[i]
+    a[i] = a[j]
+    a[j] = x
   }
-  return ret
+  return a
 }
